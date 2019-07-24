99edf1bb0ed3531c10530daca386aec99c257156
gaokaixuan <gaokaixuan@lvwan-inc.com>
2019-07-24 11:03:10 +0800

    123123

--- src/pages/index/router/detail.vue
+++ src/pages/index/router/detail.vue
@@ -155,22 +155,7 @@ export default {
            that.dec = res.data.data.describe;
            that.id = res.data.data.id;
 
-           wx.ready(function(){
-
-             let shareUrl = window.location.protocol+'//'+window.location.host+'/static/ind0.html';
-             //alert(shareUrl);
-             wx.onMenuShareAppMessage({
-                 title: sessionStorage.shareTitle, // 分享标题
-                 desc: that.dec, // 分享描述
-                 link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                 imgUrl: that.img, // 分享图标
-                 success: function () {
-                   // 设置成功
-                 }
-             });
-
-             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-           });
+           that.wxShare(sessionStorage.detailId);
           }else {
             Indicator.close();
             Toast({
@@ -180,6 +165,47 @@ export default {
           }
        })
     },
+    wxShare(e){
+      let that = this;
+
+      let desc;
+      if(that.name1!='' ||that.dec!=undefined){
+        desc = that.name1+'：'+that.dec
+      }else {
+        desc = sessionStorage.shareCon
+      }
+      wx.ready(function(){
+        let shareUrl = window.location.protocol+'//'+window.location.host+'/static/ind.html?info='+e+'&sd='+sessionStorage.appId;
+        wx.onMenuShareAppMessage({
+            title: sessionStorage.shareTitle, // 分享标题
+            desc: desc, // 分享描述
+            link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://share.marketservice.cn/uploads/20190612/16f6808112c73e4df95a44b87d1c68b0.png', // 分享图标
+            success: function () {
+              // 设置成功
+            }
+        });
+
+        wx.onMenuShareTimeline({
+           title: sessionStorage.shareTitle, // 分享时的标题
+           link: shareUrl, // 分享时的链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+           imgUrl: 'http://share.marketservice.cn/uploads/20190612/16f6808112c73e4df95a44b87d1c68b0.png', // 分享时显示的图标
+           //用户确认分享后执行的回调函数
+           success: function () {
+
+           },
+           //用户取消分享后执行的回调函数
+           cancel: function () {
+
+           }
+       });
+
+      });
+      wx.error(function(res){
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        console.log(res);
+      });
+    }
   },
   created:function(){
     this.detailFu();
